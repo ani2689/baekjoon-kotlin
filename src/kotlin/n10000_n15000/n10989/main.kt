c9d6965dab18f584ab92e1eeed27f566d35d7b85
ani2689 <ojno7777@gmail.com>
2024-03-19 21:11:19 +0900

    20240319

---
+++ src/kotlin/n10000_n15000/n10989/main.kt
@@ -0,0 +1,26 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.Scanner
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
+    val count = Integer.parseInt(br.readLine())
+
+    val num = IntArray(10001)
+
+    for (i in 0 until count)
+        num[Integer.parseInt(br.readLine())]++
+
+
+
+    for (i in 0 until 10001)
+        repeat(num[i]) {
+            bw.write("$i\n")
+        }
+
+    bw.flush()
+}
