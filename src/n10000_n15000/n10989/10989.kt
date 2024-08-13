1e60c54c4245671e90d966346d4a86254dd919eb
노현주 <dev.hyeonju@gmail.com>
2024-08-13 09:57:20 +0900

    update

--- src/n10000_n15000/n10989/10989.kt
+++ src/n10000_n15000/n10989/10989.kt
@@ -2,7 +2,6 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.Scanner
 
 fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`))
