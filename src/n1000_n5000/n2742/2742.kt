cdad16562f83a4c45f0954ca0b61b5338dd63a77
노현주 <dev.hyeonju@gmail.com>
2024-08-13 12:22:49 +0900

    2742

---
+++ src/n1000_n5000/n2742/2742.kt
@@ -0,0 +1,13 @@
+package n1000_n5000.n2742
+
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+
+    for (i in 1.. n) {
+        println("$i")
+    }
+}
